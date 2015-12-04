4b3946f0c409b58527404137af8eb156a5fdb261
romanp <romanpopov89@yahoo.com>
2015-12-04 09:44:07 +0200

    test

---
+++ src/Test/MyTest.java
@@ -0,0 +1,20 @@
+package Test;
+
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Роман
+ * Date: 26.11.15
+ * Time: 23:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyTest {
+    WebDriver driver;
+
+    WebdriverWrapper webdriverWrapper;
+    we
+
+}
